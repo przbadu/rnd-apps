db9f47ecad0c16579eecba4e2c56f0e394240beb
przbadu <pushparaj.badu@gmail.com>
2022-06-03 11:19:50 +0545

    Add support for realtime update

--- quote-editor/app/models/quote.rb
+++ quote-editor/app/models/quote.rb
@@ -4,4 +4,18 @@ class Quote < ApplicationRecord
 
   # Scopes
   scope :ordered, -> { order(id: :desc) }
+
+  # callbacks
+  # broadcast without active jobs
+  # after_create_commit -> { broadcast_prepend_to 'quotes' }
+  # after_update_commit -> { broadcast_replace_to 'quotes' }
+  # after_destroy_commit -> { broadcast_remove_to 'quotes' }
+
+  # broadcast with active jobs
+  # after_create_commit -> { broadcast_prepend_later_to 'quotes' }
+  # after_update_commit -> { broadcast_replace_later_to 'quotes' }
+  # after_destroy_commit -> { broadcast_remove_to 'quotes' }
+
+  # shorthand
+  broadcasts_to ->(_quote) { 'quotes' }, inserts_by: :prepend
 end
