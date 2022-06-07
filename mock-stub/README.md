ae287f5291413693e01fd16548d0d00be5905602
przbadu <pushparaj.badu@gmail.com>
2022-06-07 09:03:04 +0545

    Add stub example, update readme file

--- mock-stub/README.md
+++ mock-stub/README.md
@@ -8,3 +8,26 @@ This project contains list of small ruby scripts that are used to learn stub and
 bundle install
 ruby filename.rb
 ```
+
+### What is stub?
+
+Stub is a piece of code that take place of another component
+during testing. In our example, we have NumberGenerator class
+which have `random` instance method and it is using Ruby's
+`rand()` method. And our stub is taking place of `rand` method
+only and always gives us stubbed result.
+
+```ruby
+def test_random_string_by_stubbing
+  generator = NumberGenerator.new
+  generator.stubs(:rand).returns(5)
+
+  assert_equal 'AAAAA', generator.random
+end
+```
+
+### What is mock?
+
+Mocking means creating a fake version of an
+external or internal service that can stand in for the real one,
+helping your tests run more quickly and more reliably
--- mock-stub/number_generator.rb
+++ mock-stub/number_generator.rb
@@ -10,12 +10,21 @@ end
 
 # Test random number generator
 class NumberGeneratorTest < Test::Unit::TestCase
-  def test_random_string
+  # What is stub?
+  # Stub is a piece of code that take place of another component
+  # during testing. In our example, we have NumberGenerator class
+  # which have `random` instance method and it is using Ruby's
+  # `rand()` method. And our stub is taking place of `rand` method
+  # only and always gives us stubbed result.
+  def test_random_string_by_stubbing
     generator = NumberGenerator.new
-
-    # mock rand function
     generator.stubs(:rand).returns(5)
 
     assert_equal 'AAAAA', generator.random
   end
+
+  # What is mock?
+  # Mocking means creating a fake version of an
+  # external or internal service that can stand in for the real one,
+  # helping your tests run more quickly and more reliably
 end
